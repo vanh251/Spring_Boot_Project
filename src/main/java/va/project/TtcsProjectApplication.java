9e453982749e1c29fc1bda08ecab97bf44741428
vanh251 <148326521+vanh251@users.noreply.github.com>
2026-04-03 02:38:44 +0700

    first commit

---
+++ src/main/java/va/project/TtcsProjectApplication.java
@@ -0,0 +1,13 @@
+package va.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtcsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtcsProjectApplication.class, args);
+    }
+
+}
